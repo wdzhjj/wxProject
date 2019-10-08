8d70f157aafbf986058624073f8c834d02dd93e3
wdzhjj <546139504@qq.com>
2019-10-08 16:09:11 +0800

    login reg
    login reg ok

--- pages/reg/reg.js
+++ pages/reg/reg.js
@@ -1,72 +1,108 @@
-// pages/login/login.js
+// pages/reg/reg.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    username:'',
-    pwd:''
+    item: {
+      index: 0,
+      msg: '注册',
+      time: '2019-10-08'
+    },
+    code:338180,
+    email:'',
+    pwd:'',
+    repwd:'',
+    nickname:''
   },
 
-  usernameinput: function (e) {
-    var username = e.detail.value
+  emailinput:function(e){
+    var email = e.detail.value;
     this.setData({
-      username: username
+      email:email
     });
   },
-
   pwdinput: function (e) {
-    var pwd = e.detail.value
+    var pwd = e.detail.value;
     this.setData({
       pwd: pwd
     });
   },
-
-
-  submit:function(e){
-    wx.showLoading({
-      title: '登录中...',
+  repwdinput: function (e) {
+    var repwd = e.detail.value;
+    this.setData({
+      repwd: repwd
     });
-    var username = this.data.username;
+  },
+  nickinput: function (e) {
+    var nick = e.detail.value;
+    this.setData({
+      nickname: nick
+    });
+  },
+
+//注册事件
+  reg:function(e){
+    var email = this.data.email;
     var pwd = this.data.pwd;
-    console.log(username);
+    var repwd = this.data.repwd;
+    var nickname = this.data.nickname;
+    var code = this.data.code;
+
+
+    console.log(email);
     console.log(pwd);
-    if(username.length<1 || pwd.length<1){
+    console.log(nickname);
+    console.log(repwd);
+
+    if(email.length<6 || pwd.length<6 || nickname.length<1){
       wx.showToast({
-        title: '用户名密码不能为空',
+        title: '请填写完整的信息',
         icon: 'none',
         duration: 2000
       })
-      return;
     }
-    this.setData({ disabled: true });
-    wx.request({
-      url: 'http://news.hmset.com/shares/wxLogin',
-      data:{
-        username: username,
-        password: pwd
-      },
-      header:{ 'content-type':'application/json'},
-      success:function(res){
-        console.log(res.data)
-        if(res.statusCode == 200){
-          if(res.data.status == -1){
-            wx.showToast({
-              title: res.data.msg,
-              icon: 'none',
-              duration: 2000
-            })
-          }else if(res.data.status == 0){
-            wx.showToast({
-              title: res.data.msg,
-              icon: 'none',
-              duration: 2000
-            })
-            wx.redirectTo({
-              url: '../index/index'    //登录成功  跳转
-            })
+    if(pwd != repwd){
+      wx.showToast({
+        title: '两次密码不一致',
+        icon: 'none',
+        duration: 2000
+      })
+    }
+    wx.showLoading({
+      title: '注册中...',
+    });
 
+    //发送url请求
+    wx.request({
+      url: 'http://news.hmset.com/shares/wxReg',
+      data:{
+        username:email,
+        pwd:pwd,
+        nickname:nickname,
+        code:code
+      },
+      header: { 'content-type': 'application/json' },
+      success:function(res){
+        console.log(res.data);
+        if (res.statusCode == 200) {
+          if(res.data.status == 0){
+            wx.showToast({
+              title: '注册成功',
+              icon: 'none',
+              duration: 2000
+            })
+            //跳转到 login/login
+            wx.redirectTo({
+              url: '../login/login'    //登录成功  跳转
+            })
+          }else if(res.data.status == -1){
+            wx.showToast({
+              title: res.data.msg,
+              icon: 'none',
+              duration: 2000
+            })
           }else{
             wx.showToast({
               title: res.data.msg,
@@ -74,6 +110,7 @@ Page({
               duration: 2000
             })
           }
+
         }else{
           wx.showToast({
             title: '发送请求失败',
@@ -85,6 +122,7 @@ Page({
     })
 
   },
+
   /**
    * 生命周期函数--监听页面加载
    */
@@ -140,9 +178,4 @@ Page({
   onShareAppMessage: function () {
 
   }
-
 })
-
-function login(){
-  alert(1);
-}
