64ff5f17b489973ac49af6442cb7db9dee99c560
wdzhjj <546139504@qq.com>
2019-10-10 18:06:53 +0800

    10.10
    充值按钮ok

--- pages/my/my.js
+++ pages/my/my.js
@@ -7,6 +7,100 @@ Page({
   data: {
     item:{
       title:'账户详情'
+    },
+    showModalStatus: false,
+    money:[
+      100,500,1000,5000,50000
+    ],
+    way:[
+      '支付宝', '微信','银行卡'
+    ]
+  },
+
+  //绑定选择的值
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+
+  //充值金额改变
+  radioChange1:function(e) {
+    this.setData({
+      postmoney: e.detail.value
+    })
+  },
+
+  //付款方式
+  radioChange2: function (e) {
+    this.setData({
+      posttype: e.detail.value
+    })
+  },
+
+  //提交数据到后台
+  formSubmit1:function (e){
+    var self = this;
+    var token = wx.getStorageSync("SHARES_TOKEN");
+    token = 'SHARES_TOKEN=' + token;
+    var type = this.data.posttype;
+    var money = this.data.postmoney;
+    if(!type || !money){
+      wx.showToast({
+        title: '信息不全，无法提交',
+        icon: 'none',
+        duration: 2000
+      })
+      return;
+    }
+
+    wx.request({
+      url: 'http://news.hmset.com/shares/wxAddMoney',
+      data: {
+        money:money,
+        type:type
+      },
+      header: {
+        'content-type': 'application/json',
+        'Cookie': token
+      },
+      method: 'GET',
+      success: function (res) {
+        console.log(res);
+        if(res.data.status == 0){
+          wx.showToast({
+            title: res.data.msg,
+            icon: 'none',
+            duration: 2000
+          })
+          wx.redirectTo({
+            url: '../my/my',
+          })
+        }
+      }
+    })
+  },
+
+  powerDrawer:function(e){
+    var currentStatu = e.currentTarget.dataset.statu;
+    //关闭
+    if(currentStatu == 'close'){
+      console.log('close');
+      this.setData(
+        {
+          showModalStatus: false
+        }
+      );
+    }
+    // 显示 
+    if (currentStatu == "open") {
+      console.log('open');
+      this.setData(
+        {
+          showModalStatus: true
+        }
+      );
     }
   },
 
